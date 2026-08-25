843ea12286a23cde9215b90c9d3ff840aa09ce9e
Justin Musick <jd@orboro.net>
2026-08-25 10:44:06 -0400

    Release: v1.10.0 - add Stone Dragon Media footer credit, VS Code editor config tweaks

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,4 @@
-export const SITE_VERSION = "1.9.0";
+export const SITE_VERSION = "1.10.0";
 export const SITE_URL = "https://pneumarisband.com";
 export const SITE_NAME = "Pneumaris";
 export const SITE_TWITTER_HANDLE = "@pneumarisband";
